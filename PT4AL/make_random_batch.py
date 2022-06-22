e08ce19c91262dbe297be6b94a20dce60dc25a17
dinnno <sjnoh7@naver.com>
2022-06-22 05:51:35 +0000

    commit

---
+++ PT4AL/make_random_batch.py
@@ -0,0 +1,24 @@
+import random
+
+random.seed(20220622)
+
+dep_path = '/home/hinton/NAS_AIlab_dataset/personal/heo_yunjae/Parameters/Uncertainty/orderprediction'
+dest_path = '/home/hinton/NAS_AIlab_dataset/personal/heo_yunjae/Parameters/Uncertainty/random'
+
+img_paths = []
+
+for i in range(10):
+    f = open(dep_path+f'/loss/batch_{i}.txt', 'r')
+    img_paths += f.readlines()
+    
+print(len(img_paths))
+random.shuffle(img_paths)
+
+for i in range(10):
+    f = open(dest_path+f'/loss/batch_{i}.txt', 'w')
+    for j in range(900):
+        
+        path = img_paths.pop()
+        f.write(path)
+    f.close()
+        
