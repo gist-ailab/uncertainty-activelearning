9547a105b675c6058ed250c86736fe758c951767
Yunaje101 <boss9911@naver.com>
2022-07-04 04:01:02 +0000

    commit

--- PT4AL/main_rot.py
+++ PT4AL/main_rot.py
@@ -257,7 +257,8 @@ if __name__ == '__main__':
         else:
             # first iteration: sample 1k at even intervals
             samples = np.array(samples)
-            sample1k = samples[[j*5 for j in range(1000)]]
+            # sample1k = samples[[j*5 for j in range(1000)]]
+            sample1k = samples[[j for j in range(1000)]]
             # sample1k = samples[[j for j in range(900)]]
         # add 1k samples to labeled set
         labeled.extend(sample1k)
