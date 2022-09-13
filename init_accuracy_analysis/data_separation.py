8089a77ce89e51899b991461cf92fbbf5ad78178
Yunaje101 <boss9911@naver.com>
2022-09-13 13:01:21 +0900

    commit

--- init_accuracy_analysis/data_separation.py
+++ init_accuracy_analysis/data_separation.py
@@ -1,1 +1,8 @@
 import glob
+import torch
+import torch.nn.functional as F 
+from torch.optim import Adam
+from torchvision.models import resnet18
+from torch.utils.data import dataloader
+from torch.nn import CrossEntropyLoss
+
