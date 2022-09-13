be23809b3aa64ffc339c3e49606237bd34217d1f
Yunaje101 <boss9911@naver.com>
2022-09-13 13:02:43 +0900

    commit

--- init_accuracy_analysis/data_separation.py
+++ init_accuracy_analysis/data_separation.py
@@ -1,4 +1,4 @@
-import glob
+from glob import glob
 import torch
 import torch.nn.functional as F 
 from torch.optim import Adam
@@ -6,3 +6,7 @@ from torchvision.models import resnet18
 from torch.utils.data import dataloader
 from torch.nn import CrossEntropyLoss
 
+data_path = ''
+
+#get subset
+filenames = glob(data_path+'/*')
